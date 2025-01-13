780fb53d543e7ba25edcb47b2afc62ef80d1aab7
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-01-13 10:19:37 +0000

    Upgrade ESLint to v9

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import cyf from "@codeyourfuture/eslint-config-standard";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+	cyf,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			globals: globals.node,
+			parserOptions: {
+				project: true,
+			},
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: "error",
+		},
+	},
+	{
+		files: ["**/*.js"],
+		...tseslint.configs.disableTypeChecked,
+	},
+	{
+		files: ["**/*.test.ts"],
+		rules: {
+			"@typescript-eslint/no-floating-promises": "off",
+			"@typescript-eslint/no-non-null-assertion": "off",
+		},
+	},
+	{
+		ignores: ["packages/atlas-ip/lib", "packages/action/main", "packages/action/post"],
+	},
+];
